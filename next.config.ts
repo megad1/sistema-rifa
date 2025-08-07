66adbc408bacfd91476f483bf35438a22d94f744
ives7k <clandestino7k@gmail.com>
2025-08-07 09:55:58 -0300

    Adiciona configuração de CORS para permitir requisições da API qrserver.com, melhorando a integração com serviços externos.

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,12 @@ const nextConfig: NextConfig = {
         hostname: 'incs-bucket.s3.us-east-1.amazonaws.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.qrserver.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
