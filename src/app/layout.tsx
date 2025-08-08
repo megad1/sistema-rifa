cae0f6cb5848e8c8179aabe73865d53514ae8e2e
IVES <165990522+ives7k@users.noreply.github.com>
2025-08-08 20:36:23 -0300

    feat: Refatora a implementação do Facebook Pixel utilizando o componente Script do Next.js, melhorando a performance e a acessibilidade do código.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { getFacebookSettings } from '@/lib/facebook';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,9 +24,17 @@ export default async function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         {fb.enabled && fb.pixelId && (
           <>
-            <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '${fb.pixelId}'); fbq('consent', 'grant'); fbq('track','PageView');` }} />
-            {/* eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text */}
-            <img height="1" width="1" style={{display:'none'}} src={`https://www.facebook.com/tr?id=${fb.pixelId}&ev=PageView&noscript=1`} alt="" />
+            <Script id="fb-pixel" strategy="afterInteractive">{`
+              !function(f,b,e,v,n,t,s){
+                if(f.fbq)return; n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n; n.push=n; n.loaded=!0; n.version='2.0'; n.queue=[];
+                t=b.createElement(e); t.async=!0; t.src=v; s=b.getElementsByTagName(e)[0]; s.parentNode.insertBefore(t,s)
+              }(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${fb.pixelId}');
+              fbq('consent','grant');
+              fbq('track','PageView');
+            `}</Script>
+            <noscript dangerouslySetInnerHTML={{ __html: `<img height=\"1\" width=\"1\" style=\"display:none\" src=\"https://www.facebook.com/tr?id=${fb.pixelId}&ev=PageView&noscript=1\" alt=\"\" />` }} />
           </>
         )}
       </head>
