2057fb760b4223092d584c1c98bd76c87889e6cf
IVES <165990522+ives7k@users.noreply.github.com>
2025-08-08 19:58:36 -0300

    feat: Remove padrões remotos de imagens no next.config.ts e ativa a opção unoptimized para permitir qualquer domínio.

--- next.config.ts
+++ next.config.ts
@@ -2,39 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'incs-bucket.s3.amazonaws.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 's3.incrivelsorteios.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'incs-bucket.s3.us-east-1.amazonaws.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'api.qrserver.com',
-        port: '',
-        pathname: '/**',
-      }
-      ,
-      {
-        protocol: 'https',
-        hostname: 'blog.autocompara.com.br',
-        port: '',
-        pathname: '/**',
-      }
-    ],
+    unoptimized: true, // permite qualquer domínio (desativa otimização do Next)
   },
 };
 
